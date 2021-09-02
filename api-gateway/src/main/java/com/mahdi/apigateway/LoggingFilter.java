48c79c7299fa2348528c37d22abc424af45eec13
mahdizaabi <mahdi.zaabi@gmail.com>
2021-09-02 17:49:39 +0200

    add global logger to the gateway

---
+++ api-gateway/src/main/java/com/mahdi/apigateway/LoggingFilter.java
@@ -0,0 +1,31 @@
+package com.mahdi.apigateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+
+@Component
+public class LoggingFilter implements GlobalFilter {
+    /**
+     * Process the Web request and (optionally) delegate to the next {@code WebFilter}
+     * through the given {@link GatewayFilterChain}.
+     *
+     * @param exchange the current server exchange
+     * @param chain    provides a way to delegate to the next filter
+     * @return {@code Mono<Void>} to indicate when request processing is complete
+     */
+
+    private Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("Path of the received request is:-> {}",exchange.getRequest().getPath());
+        return chain.filter(exchange);
+    }
+}
