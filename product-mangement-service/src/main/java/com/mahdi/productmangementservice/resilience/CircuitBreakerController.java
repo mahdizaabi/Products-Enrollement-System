5ec26156f1c948182b54a6ec8a8d6207e8290db8
mahdizaabi <mahdi.zaabi@gmail.com>
2021-09-03 14:33:32 +0200

    enable circuit breaker

--- product-mangement-service/src/main/java/com/mahdi/productmangementservice/resilience/CircuitBreakerController.java
+++ product-mangement-service/src/main/java/com/mahdi/productmangementservice/resilience/CircuitBreakerController.java
@@ -1,6 +1,7 @@
 package com.mahdi.productmangementservice.resilience;
 
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,16 +12,31 @@ import org.springframework.web.client.RestTemplate;
 
 
 /*
-for whatever reason the controller fail,
+ @Retry: for whatever reason the controller fail,
 the Retry() annotation will retry the call to the controlelr 3 time(default configuration)
+
+- @Circuit breaker: the circuit breaker when detects the fail of a service, circuit breaker will invoke the
+fallbackMethod without reaching / calling the microservice.
+=>it return the default response from the fallbackmethod directly
+3 states of the circuit breaker
+starts closed than when no proper response are coming from the service
+it switches to open to stop all incoming requests and send defaul fall back response instead,
+then after a while it move to the semi open state to let only a spécific portion o fthe request to be passed to the microservice
+if it detects a healthy response from the service it switchback to closed state,
+if not it switch back to closee.
+____ __ ____ closeed: closed state, the microservice will be allways called
+___ |______ open state: No request are passed to the microservice
+___ \_____  semi-open state when the circuit breaker let only on portion of request to be passed to the microservice
 */
 
 @RestController
 public class CircuitBreakerController {
 
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api", fallbackMethod = "fallbackmethod")
+    //@Retry(name = "sample-api", fallbackMethod = "fallbackmethod")
+    @CircuitBreaker(name = "default", fallbackMethod = "fallbackmethod")
     public String sampleApi() {
         logger.info("Sample api call received");
         ResponseEntity<String> response = new RestTemplate().getForEntity("http://localhost:8080/okokok", String.class);
